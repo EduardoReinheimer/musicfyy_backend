192a6bda3ffb49a73dd9349c0c3a62bdf59a84f3
Eduardo Salvador Reinheimer <du428@hotmail.com>
2020-11-17 15:40:20 -0300

    correções a pasta static

--- api/urls.py
+++ api/urls.py
@@ -13,16 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path , include , re_path
+from django.conf.urls.static import static
+from django.conf.urls import url
 from django.conf import settings
+from allauth.account.views import confirm_email
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
-    re_path('', include('users.urls')),
     path('admin/', admin.site.urls),
     re_path('api/(?P<version>(v1|v2))/', include('music.urls')),
+
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^account/', include('allauth.urls')),
+    url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
     url(r'^api/v1/auth/obtain_token/', obtain_jwt_token),
     url(r'^api/v1/auth/refresh_token/', refresh_jwt_token),
-]  # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
